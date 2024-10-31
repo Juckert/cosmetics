6589f9eaa5a808e37637ad26c3162a07228645df
Artem-Kodland <134316152+Artem-Kodland@users.noreply.github.com>
2024-10-31 17:48:20 +0500

    Update init

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-# python.exe -m pip install --upgrade pip
-# pip install pytesseract
-
 from PIL import Image
 import pytesseract
+import logging
 
 
-class DoWork:
-    def __init__(self, file: str, lang='rus+eng', path='pic/'):
-        self.lang = lang
-        self.path = path
-        self.file = file
+class TextExtractor:
+    def __init__(self, image_path: str):
+        self.image_path = image_path
+        self.processed_image = None
+        self.image = None
+
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
     def its(self):
         return pytesseract.image_to_string(Image.open(self.path + self.file), self.lang)
 
 
 if __name__ == '__main__':
-    print('Use DoWork interface')
+    do = TextExtractor('1.jpg')
+    print(do.its())
+    
