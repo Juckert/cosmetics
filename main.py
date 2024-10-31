d1f9b6f2d8810c21fa43cf2419fcc60fff3670a4
Juckert <juckertartem@gmail.com>
2024-10-31 18:08:36 +0500

    Add preprocess_image

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pytesseract
 import logging
 import cv2
 
+
 class TextExtractor:
     def __init__(self, image_path: str):
         self.image_path = image_path
@@ -10,6 +11,7 @@ class TextExtractor:
         self.image = None
 
         logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        
 
     def load_image(self):
         """Загрузка изображения из файла."""
@@ -18,6 +20,21 @@ class TextExtractor:
         if self.image is None:
             logging.error(f"Не удалось загрузить изображение: {self.image_path}")
             raise FileNotFoundError(f"Не удалось загрузить изображение: {self.image_path}")
+        
+
+    def preprocess_image(self, blur: bool = False, adaptive: bool = False):
+        """Преобразование изображения в оттенки серого и бинаризация."""
+        logging.info("Предобработка изображения: преобразование в серые тона и бинаризация.")
+        gray_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+
+        if blur:
+            gray_image = cv2.medianBlur(gray_image, 3)
+
+        if adaptive:
+            self.processed_image = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                                         cv2.THRESH_BINARY_INV, 11, 2)
+        else:
+            _, self.processed_image = cv2.threshold(gray_image, 150, 255, cv2.THRESH_BINARY_INV)
 
 
 if __name__ == '__main__':
