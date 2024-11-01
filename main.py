5d2c33ac95cfe7c84c2087c67c93a458c19b8411
Juckert <juckertartem@gmail.com>
2024-11-01 09:51:46 +0500

    Add load_image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pytesseract
 import logging
+import cv2
 
 pytesseract.pytesseract.tesseract_cmd = r'Путь до tesseract'
 
@@ -10,3 +11,11 @@ class TextExtractor:
         self.image = None
 
         logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    def load_image(self):
+        """Загрузка изображения из файла."""
+        logging.info(f"Загрузка изображения из {self.image_path}")
+        self.image = cv2.imread(self.image_path)
+        if self.image is None:
+            logging.error(f"Не удалось загрузить изображение: {self.image_path}")
+            raise FileNotFoundError(f"Не удалось загрузить изображение: {self.image_path}")
