43f7028ee38ab1a8a502129e4b5d55afc3604c0e
olllleg <147877452+olllleg@users.noreply.github.com>
2025-01-17 14:00:25 +0500

    Delete keyboards.py

--- keyboards.py
+++
@@ -1,4 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='user_info')],[KeyboardButton(text='help')]]
-                           )
